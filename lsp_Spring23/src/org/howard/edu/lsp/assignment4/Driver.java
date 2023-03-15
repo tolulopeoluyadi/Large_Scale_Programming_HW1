62bd41c2d11aab9d4e10e9b7233c1e3cb1ab9513
tolulopeoluyadi <109180539+tolulopeoluyadi@users.noreply.github.com>
2023-03-15 19:45:18 -0400

    updating driver class

--- lsp_Spring23/src/org/howard/edu/lsp/assignment4/Driver.java
+++ lsp_Spring23/src/org/howard/edu/lsp/assignment4/Driver.java
@@ -13,20 +13,61 @@ public static void main(String[] args) throws IntegerSetException {
     set1.add(1);
     set1.add(2);
     set1.add(3);
-
+    
+    IntegerSet set4 = new IntegerSet();
+    set4.add(1);
+    set4.add(2);
+    set4.add(3);
+    
     System.out.println("Value of Set1 is: " + set1.toString());
     System.out.println("Smallest value in Set1 is: " + set1.smallest());
     System.out.println("Largest value in Set1 is: " + set1.largest());
+    
+    System.out.println("set1 contains 2? " + set1.contains(2));
+    System.out.println("set1 contains 4? " + set1.contains(4));
 
     IntegerSet set2 = new IntegerSet();
+    set2.add(2);
     set2.add(4);
     set2.add(5);
+    
+    IntegerSet set3 = new IntegerSet();
+    set3.add(1);
+    set3.add(2);
+    set3.add(3);
+    
+    
+    System.out.println("set1 equals set4? " + set1.equals(set4)); // expected output: true
+    System.out.println("set1 equals set3? " + set1.equals(set3)); 
 
     System.out.println("Union of Set1 and Set2");
     System.out.println("Value of Set1 is: " + set1.toString());
     System.out.println("Value of Set2 is: " + set2.toString());
-    set1.union(set2); // union of set1 and set2
+    set1.union(set2);
     System.out.println("Result of union of Set1 and Set2");
-    System.out.println(set1.toString()); // result of union of set1 and set2
+    System.out.println(set1.toString());
+    
+    System.out.println("Intersection of Set1 and Set2");
+    System.out.println("Value of Set1 is: " + set1.toString());
+    System.out.println("Value of Set3 is: " + set3.toString());
+    set1.intersect(set3);
+    System.out.println("Result of intersection of Set1 and Set3");
+    System.out.println(set1.toString());
+
+    System.out.println("Difference of Set1 and Set3");
+    System.out.println("Value of Set1 is: " + set1.toString());
+    System.out.println("Value of Set3 is: " + set3.toString());
+    set1.diff(set3);
+    System.out.println("Result of difference of Set1 and Set3");
+    System.out.println(set1.toString());
+    
+    System.out.println("clear Set1");
+    set1.clear(); 
+    System.out.println("Value of Set1 is: " + set1.toString());
+    System.out.println("Is Set1 empty? " + set1.isEmpty()); 
+    
+	set2.remove(2);
+    System.out.println("set2 after removing 2: " + set2.toString());
 }
 }
+
